75634ae0cb8e2a0b6b96cd8e4f13e03197e3fe16
anguianoo <njanguiano@gmail.com>
2022-07-21 09:49:36 -0700

    added class CashRegister

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
-
+class CashRegister
+  # getter, setter
+  attr_accessor :total, :discount, :items, :last_transaction
+  
+  # initialize total, discount, items
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []    
+  end
+  
+  # CashRegister.add_item("pizza", 2.50, 2)
+  def add_item(title, amount, quantity = 1)
+    self.last_transaction = amount * quantity
+    self.total += self.last_transaction
+    quantity.times do
+      self.items << title
+    end
+  end
+  
+  def apply_discount
+    if self.discount != 0
+      discount_as_percent = (100.0 - self.discount.to_f) / 100
+      self.total = (self.total * discount_as_percent).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply"
+    end
+  end
+  
+  def void_last_transaction
+    self.total -= self.last_transaction
+  end
+  
+end
